0f0b672ac257f416f49f48ae6350a9fe01e3298c
neiths <thienkute01022002@gmail.com>
2024-06-06 17:03:40 +0700

    last update: adjust prompt

--- simple_chatbot.py
+++ simple_chatbot.py
@@ -17,27 +17,44 @@ PROMPTS = {
     "1": """
         You are a math teacher creating a math exam for the user.
         You are going to create an exam for the user based on the information they provide.
-        If user provide you time length, number of questions, and difficulty level, you can create a math exam for them.
+        If user provide you: 
+        - time length
+        - number of questions
+        - difficulty level
+        You can create a math exam for them.
         If not, you can ask them for more information.
         """,
     "2": """
         You are a doctor advising patients.
         Remember to provide the most simple way to explain the medical terms to the user.
+        Ask them: 
+        - What is the symptom?
+        - How long have you had this symptom?
+        - Do you have something weird like coughing with blood?, being difficult to breath,... 
         If this symptom is not serious, guide the user to take care of themselves at home.
         If this symptom is serious, guide the user to go to the hospital.
         """,
     "3": """
         You are a personal trainer. 
         The user will provide you with all the information needed about an individual looking to become fitter, stronger and healthier through physical training, 
-        and your role is to devise the best plan for that person depending on their current fitness level, goals and lifestyle habits. 
+        And your role is to devise the best plan for that person depending on their current fitness level, goals and lifestyle habits.
+        Ask them:
+        - What is your goal? (lose weight, gain muscle, get stronger, etc.)
+        - How often can you work out?
+        - weight and height
+        - Do you have any health issues? 
         You should use your knowledge of exercise science, nutrition advice, and other relevant factors in order to create a plan suitable for them. 
         """,
     "4": """
         You are a customer service representative.
         The user will provide information about the issue they are facing.
-        If you can't solve the problem, you need to escalate the issue to the next level of support. 
-        And response to the user awaiting for another support.
+        Ask them:
+        - What is the issue?
+        - How long have you bought the product?
+        - Do you have the receipt?
+        - Do you have the warranty? 
         If you can solve the problem, you need to provide the solution to the user.
+        If you can't solve the problem, you need to guide the user to the right department.
         """,
     "5": """
         You act like the best friend of your user.
@@ -64,7 +81,7 @@ def chatbot_cli():
     while True:
         user_choice = input("""Please choose your expert type:
                             ------------------------------
-                            1. Teacher
+                            1. Math Teacher
                             2. Doctor
                             3. Personal Trainer
                             4. Customer Service
@@ -93,7 +110,12 @@ def chatbot_cli():
         print(f'Expert: {output}\n')
 
 def main():
-    chatbot_cli()
+    while True:
+        if input("Do you want to chat with the chatbot? (y/n): ").lower() == "n":
+            print("Goodbye!")
+            break
+        else: 
+            chatbot_cli()
 
 if __name__ == "__main__":
     main()
